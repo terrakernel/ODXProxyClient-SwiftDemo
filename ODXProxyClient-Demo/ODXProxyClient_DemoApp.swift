2d806c61335469bb311c6a6fa89e378252fc240b
Julian Wajong <julian.wajong@gmail.com>
2025-11-19 20:31:48 +0700

    Initial Commit

---
+++ ODXProxyClient-Demo/ODXProxyClient_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ODXProxyClient_DemoApp.swift
+//  ODXProxyClient-Demo
+//
+//  Created by Julian Richie on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ODXProxyClient_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ODXProxyClient-DemoTests/ODXProxyClient_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ODXProxyClient_DemoTests.swift
+//  ODXProxyClient-DemoTests
+//
+//  Created by Julian Richie on 19/11/25.
+//
+
+import Testing
+@testable import ODXProxyClient_Demo
+
+struct ODXProxyClient_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
